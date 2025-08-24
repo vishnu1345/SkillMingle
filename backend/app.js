a9c01cc76a7f2ca863c2ddcc18db80d919d4c2d4
Vishnu1345 <vishnu149816@gmail.com>
2025-08-25 01:25:40 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 
 
 const collection = require("./mongo");
